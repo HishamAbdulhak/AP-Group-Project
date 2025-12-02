cc23b42ce55d60127afc1c054b03110b5208f085
HishamAbdulhak <hisham0abdulhak@gmail.com>
2025-12-02 22:25:06 +0800

    UI Hotfix: Add Settings & Back Button in Admin Dashboard
    - Implemented Settings button to Admin Dashboard
    - Created SettingsPageController to provide future functionality to the settings page
    - Implemented Back button to return to Admin Dashboard
    - Improved navigation consistency and user experience

--- src/main/java/groupproject/apgroupproject/controllers/SettingsPageController.java
+++ src/main/java/groupproject/apgroupproject/controllers/SettingsPageController.java
@@ -3,14 +3,14 @@ package groupproject.apgroupproject.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class AdminController extends BaseController{
-
-    @FXML private Button adminSettings;
+public class SettingsPageController extends BaseController{
+    @FXML private Button backButton;
 
     @FXML
     public void initialize() {
         super.setupSidebar();
 
-        adminSettings.setOnAction(e -> navService.navigateTo("SettingsPage.fxml", adminSettings));
+        backButton.setOnAction(e -> navService.navigateTo("AdminDashboard.fxml", backButton));
     }
 }
+
